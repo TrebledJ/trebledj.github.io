eddd22d22125dbfc7523ffcfb2e90f7bc362c017
Johnathan <jjjlaw@connect.ust.hk>
2022-07-25 18:00:42 +0800

    fix mode switch

--- assets/js/mode-switcher.js
+++ assets/js/mode-switcher.js
@@ -1,6 +1,15 @@
 (() => {
-  document.documentElement.setAttribute('data-theme', 'dark');
-  sessionStorage.setItem('theme', 'dark');
+  let currentTheme = sessionStorage.getItem("theme");
+  if (currentTheme === "dark") {
+    document.documentElement.setAttribute("data-theme", "dark");
+    sessionStorage.setItem("theme", "dark");
+  } else if (currentTheme === "light") {
+    document.documentElement.setAttribute("data-theme", "light");
+    sessionStorage.setItem("theme", "light");
+  } else {
+    document.documentElement.setAttribute("data-theme", "dark");
+    sessionStorage.setItem("theme", "dark");
+  }
 })();
 
 function modeSwitcher() {
