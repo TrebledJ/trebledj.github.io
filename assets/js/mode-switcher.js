bdaa8741acb45cf24eb6e464bba45b6b05ec5e71
Johnathan <jjjlaw@connect.ust.hk>
2022-07-25 10:11:02 +0800

    throw out a bunch of template stuff, good riddance

--- assets/js/mode-switcher.js
+++ assets/js/mode-switcher.js
@@ -1,19 +1,19 @@
-
-(()=> {	modeSwitcher()})();
+// (() => {
+//   modeSwitcher();
+// })();
 
 function modeSwitcher() {
-	document.documentElement.setAttribute('data-theme', 'dark');
-	sessionStorage.setItem('theme', 'dark');
+  // document.documentElement.setAttribute('data-theme', 'dark');
+  // sessionStorage.setItem('theme', 'dark');
+  let currentTheme = sessionStorage.getItem("theme");
+  if (currentTheme === "dark") {
+    document.documentElement.setAttribute("data-theme", "light");
+    sessionStorage.setItem("theme", "light");
+  } else if (currentTheme === "light") {
+    document.documentElement.setAttribute("data-theme", "dark");
+    sessionStorage.setItem("theme", "dark");
+  } else {
+    document.documentElement.setAttribute("data-theme", "dark");
+    sessionStorage.setItem("theme", "dark");
+  }
 }
-
-// 	if (currentTheme === "dark") {
-// 		document.documentElement.setAttribute('data-theme', 'light');
-// 		sessionStorage.setItem('theme', 'light');
-// 	}	else if (currentTheme === "light") {
-// 		document.documentElement.setAttribute('data-theme', 'dark');
-// 		sessionStorage.setItem('theme', 'dark');
-// 	}else{
-// 		document.documentElement.setAttribute('data-theme', 'dark');
-// 		sessionStorage.setItem('theme', 'dark');
-// 	}
-// }
