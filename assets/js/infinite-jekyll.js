a699c54983abb7ad8bf30cebb2fd26c946831546
Johnathan <jjjlaw@connect.ust.hk>
2022-07-26 16:08:31 +0800

    refactor music loading to auto discover posts with `music` tag

--- assets/js/infinite-jekyll.js
+++ assets/js/infinite-jekyll.js
@@ -1,27 +1,38 @@
-$(function() {
-  let items,
-      isFetchingPosts = false,
-      shouldFetchPosts = true,
-      itemsToLoad = $("#track-list li").length, // Count existing number of items.
-      loadedItems = itemsToLoad;
+let items,
+    itemsToLoad,
+    isFetchingPosts = false,
+    shouldFetchPosts = true,
+    loadedItems = itemsToLoad;
 
-  // Load the JSON file containing all URLs
-  $.getJSON('/assets/json/all-tracks.json', function(data) {
-    items = data["tracks"];
-    
-    // If there aren't any more items available to load than already visible, disable fetching
-    if (items.length <= itemsToLoad)
-      disableFetching();
-  });
+let baseurl;
 
-  // If there's no spinner, it's not a page where items should be fetched
-  if ($(".infinite-spinner").length < 1)
-    shouldFetchPosts = false;
-	
-  // Are we close to the end of the page? If we are, load more items
-  $(window).on('scroll', function(e){
+// Get site metadata.
+$.getJSON('/assets/json/metadata.json', data => {
+  baseurl = data['baseurl'];
+})
+
+// Load the JSON file containing all URLs
+$.getJSON('/assets/json/all-tracks.json', data => {
+  items = data["tracks"];
+  
+  // If there aren't any more items available to load than already visible, disable fetching
+  if (items.length <= itemsToLoad)
+    disableFetching();
+  
+  fetchPosts();
+});
+
+// If there's no spinner, it's not a page where items should be fetched
+if ($(".infinite-spinner").length < 1)
+  shouldFetchPosts = false;
+
+function loadInfinite(num) {
+  itemsToLoad = num;
+  fetchPosts();
+  $(window).on('scroll', e => {
     if (!shouldFetchPosts || isFetchingPosts || loadedItems >= items.length) return;
     
+    // Are we close to the end of the page? If we are, load more items
     let scrollPercentage = 100 * $(window).scrollTop() / ($(document).height() - $(window).height());
 
     // If we've scrolled past the loadNewPostsThreshold, fetch items
@@ -29,44 +40,57 @@ $(function() {
       fetchPosts();
     }
   });
-  
-  // Fetch a chunk of items
-  function fetchPosts() {
-    // Exit if items haven't been loaded
-    if (!items) return;
-    
-    isFetchingPosts = true;
-    
-    // Load as many items as there were present on the page when it loaded
-    // After successfully loading a post, load the next one
-    for (let i = 0; i < itemsToLoad; i++) {
-      let index = loadedItems + 1;
-      if (index >= items.length) {
-        break;
-      }
-      
-      fetchPostWithIndex(index);
-      loadedItems++;
-    }
-    disableFetching();
-  }
-	
-  function fetchPostWithIndex(index) {
-    var item = items[index];
+}
 
-    $([
-      "<li>",
-      " <hr/>",
-      "   <iframe allow=\"autoplay\" frameborder=\"no\" height=\"166\" scrolling=\"no\"",
-      "     src=\"https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/" + item + "&color=%233e45c5&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true\"",
-      "     width=\"100%\"></iframe>",
-      "</li>",
-    ].join("\n")).appendTo("#track-list")
+// Fetch a chunk of items
+function fetchPosts() {
+  // Exit if items haven't been loaded
+  if (!items) {
+    return;
   }
   
-  function disableFetching() {
-    isFetchingPosts = false;
-    $(".infinite-spinner").fadeOut();
+  // Wait for loadInfinite to be called
+  if (!itemsToLoad) {
+    return;
   }
-	
-});
+  
+  isFetchingPosts = true;
+  
+  // Load as many items as there were present on the page when it loaded
+  // After successfully loading a post, load the next one
+  for (let i = 0; i < itemsToLoad; i++) {
+    let index = loadedItems;
+    if (index >= items.length) {
+      break;
+    }
+    
+    fetchPostWithIndex(index);
+    loadedItems++;
+  }
+  disableFetching();
+}
+
+function fetchPostWithIndex(index) {
+  let item = items[index];
+  let tags = item.tags.map(t => `&nbsp;•&nbsp;&nbsp;<a class="post-tags" href="${baseurl}/tags/${t}">${t}</a>`).join("\n");
+
+  $([
+    ` <li>
+        <br/>
+        <iframe allow="autoplay" frameborder="no" height="166" scrolling="no"
+          src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/${item.track_id}&color=%233e45c5&auto_play=false&hide_related=true&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"
+          width="100%"></iframe>
+        <div>
+          <span class="post-date">${item.date}</span>
+          &nbsp;•&nbsp;
+          <a class="post-meta" href="${baseurl}${item.url}">Preface</a>
+          ${tags}
+        </div>
+      </li>`,
+  ]).appendTo("#track-list")
+}
+
+function disableFetching() {
+  isFetchingPosts = false;
+  $(".infinite-spinner").fadeOut();
+}
