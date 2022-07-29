a6fe26222a473456ee8c406ff0c99d0d210c5782
Johnathan <jjjlaw@connect.ust.hk>
2022-07-30 00:13:59 +0800

    fix search bar up a bit

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,44 +1,40 @@
 $(document).ready(function() {
-    'use strict';
-    var search_field = $('.search-form__field'),
-        search_results = $('.search-results'),
-        toggle_search = $('.toggle-search-button'),
-        close_search = $('.close-search-button'),
-        search_result_template = "\
-          <div class='search-results__item'>\
-            <a class='search-results__item__title' href='{{link}}'>{{title}}</a>\
-            <span class='post__date'>{{pubDate}}</span>\
-          </div>";
+  'use strict';
+  var search_field = $('.search-box'),
+      results_list = $('#results-list'),
+      toggle_search = $('#toggle-search-button'),
+      search_result_template = "\
+        <div class='results-list__item'>\
+          <a class='results-list__item__title' href='{{link}}'>{{title}}</a>\
+          <span class='post__date'>{{pubDate}}</span>\
+        </div>";
 
-    toggle_search.click(function(event) {
-      event.preventDefault();
-      $('.search-form-container').addClass('is-active');
+  toggle_search.click(function(event) {
+    event.preventDefault();
+    $('.search-form-container').toggleClass('is-active');
+    // TODO: change search icon to X.
+    setTimeout(function() {
+      $().focus();
+    }, 400);
+  })
 
-      setTimeout(function() {
-        search_field.focus();
-      }, 500);
-    });
-
-    $('.search-form-container').on('keyup', function(event) {
-      if (event.keyCode == 27) {
-        $('.search-form-container').removeClass('is-active');
-      }
-    });
-
-    $('.close-search-button').click(function() {
+  $('.search-form-container').on('keyup', function(event) {
+    // Escape key.
+    if (event.keyCode == 27) {
       $('.search-form-container').removeClass('is-active');
-    });
-
-    search_field.ghostHunter({
-      results: search_results,
-      onKeyUp         : true,
-      rss             : base_url + '/feed.xml',
-      zeroResultsInfo : false,
-      info_template   : "<h4 class='heading'>Number of posts found: {{amount}}</h4>",
-      result_template : search_result_template,
-      before: function() {
-        search_results.fadeIn();
-      }
-    });
-
+    }
   });
+
+  search_field.ghostHunter({
+    results: results_list,
+    onKeyUp         : true,
+    rss             : base_url + '/feed.xml',
+    zeroResultsInfo : true,
+    info_template   : "<h4 class='heading'>Number of posts found: {{amount}}</h4>",
+    result_template : search_result_template,
+    before: function() {
+      results_list.fadeIn();
+    }
+  });
+
+});
