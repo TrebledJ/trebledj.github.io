85f37dd9027a6005844b89093aa56d8aa2c80166
Johnathan <jjjlaw@connect.ust.hk>
2022-08-02 06:53:41 +0800

    lessen my ocd

--- assets/js/infinite-jekyll.js
+++ assets/js/infinite-jekyll.js
@@ -73,11 +73,13 @@ function fetchPostWithIndex(index) {
         <iframe allow="autoplay" frameborder="no" height="166" scrolling="no"
           src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/${item.track_id}&color=%23${soundcloud_color}&auto_play=false&hide_related=true&show_comments=true&show_user=true&show_reposts=false&show_teaser=false"
           width="100%"></iframe>
-        <div>
+        <div class="d-flex flex-row align-items-center">
           <span class="post-date"><i class="fas fa-calendar post-meta-icon me-1"></i>${item.date}</span>
-          &nbsp;•&nbsp;
+          <span class="fs-6">&nbsp;•&nbsp;</span>
           <span><i class="fas fa-tag post-meta-icon me-1"></i></span>
+          <div>
           ${tags}
+          </div>
         </div>
         <div class="post-meta">
           <a href="${base_url}${item.url}">Preface</a>:
