826a46daafb30b06b5e46ed6745c2c081279c13f
Johnathan <jjjlaw@connect.ust.hk>
2022-08-08 19:21:23 +0800

    forgot to remove

--- assets/js/head.js
+++ assets/js/head.js
@@ -6,15 +6,3 @@ $(document).ready(function() {
         $(".post-posted-date").text(new Date().toISOString().split('T')[0]);
     }
 });
-
-$(document).ready(function() {
-    let article = $(".post-article");
-    if (!article) {
-        console.log("This page is not an article! Buh bye.");
-    } else {
-        console.log("oooh, it's an article!")
-    }
-    for (obj of $(".post-article pre, .post-article code")) {
-        console.log(obj.contents());
-    }
-})
