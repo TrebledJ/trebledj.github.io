dc73c201381eb9fd2df5f4bc5c817d263d81ae8b
Johnathan <jjjlaw@connect.ust.hk>
2022-08-08 23:32:25 +0800

    fix 404 randomisation

--- assets/js/head.js
+++ assets/js/head.js
@@ -1,6 +1,6 @@
 // --- 404 random meme --- //
-$(document).ready(function() {
-    if ("{{page.title}}" === "404") {
+$(document).ready(function () {
+    if ($("title")[0].innerText.includes("404")) {
         let img = Math.floor(Math.random() * 3) + 1;
         $(`#img-404-${img}`).removeClass("d-none");
         $(".post-posted-date").text(new Date().toISOString().split('T')[0]);
