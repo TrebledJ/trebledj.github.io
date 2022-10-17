4d2a9df11062a10b2a2311c4657252880fc069ca
Johnathan <jjjlaw@connect.ust.hk>
2022-10-17 17:10:46 +0800

    more me mes

--- assets/js/head.js
+++ assets/js/head.js
@@ -1,7 +1,8 @@
 // --- 404 random meme --- //
 $(document).ready(function () {
+    var numMemes = 4;
     if ($("title")[0].innerText.includes("404")) {
-        let img = Math.floor(Math.random() * 3) + 1;
+        let img = Math.floor(Math.random() * numMemes) + 1;
         $(`#img-404-${img}`).removeClass("d-none");
         $(".post-posted-date").text(new Date().toISOString().split('T')[0]);
     }
