72a29d11e67b04d4281682ff3893f908ff6fc81e
Johnathan <jjjlaw@connect.ust.hk>
2023-02-01 01:28:17 +0800

    smol cleanup

--- _data/site.js
+++ _data/site.js
@@ -2,37 +2,37 @@ module.exports = function () {
 	const email = "trebledjjj@gmail.com"
 
 	return {
-		"title": "TrebledJ's Pages",
+		title: "TrebledJ's Pages",
 		subtitle: "Dabbling in code, music, math, and memes since conception.",
-		"url": "https://trebledj.github.io",
-		"language": "en",
-		"description": "TrebledJ's personal blog on programming, music, and memes.",
-		"author": {
-			"alias": "TrebledJ",
-			"name": "Johnathan",
-			"email": email,
-			"logo": "profile.png",
-			"bio": `Hi! I'm a final year HKUST mathematics undergrad.
+		url: "https://trebledj.github.io",
+		language: "en",
+		description: "TrebledJ's personal blog on programming, music, and memes.",
+		author: {
+			alias: "TrebledJ",
+			name: "Johnathan",
+			email: email,
+			logo: "profile.png",
+			bio: `Hi! I'm a final year HKUST mathematics undergrad.
 			I'm passionate about [programming](/tags/programming) and [composing music](/tags/composition).
 			I'm also interested in a wide range of fields: [robotics](/tags/robotics), [cybersecurity](/tags/ctf),
 			[application development](/tags/apps), and software engineering in general.
 			I'm excited about developing applications to solve problems and provide an educational interface for learners.`
 		},
-		"navpages": [
+		navpages: [
 			{ text: 'Home', url: '/' },
 			{ text: 'Posts', url: '/posts' },
 			{ text: 'Projects', url: '/tags/project' },
 			{ text: 'Music', url: '/music' },
 		],
-		"github_username": "TrebledJ",
-		"musescore_id": "20636901",
-		"google_analytics": "G-7TQ4WV0LMK",
-		"disqus_shortname": "trebledj",
-		"soundcloud_color": "2631c8",
-		"social": [
+		github_username: "TrebledJ",
+		musescore_id: "20636901",
+		google_analytics: "G-7TQ4WV0LMK",
+		disqus_shortname: "trebledj",
+		soundcloud_color: "2631c8",
+		social: [
 			{ name: "GitHub", icon: "fab fa-github", color: 'rgb(150, 60, 180)', link: "http://github.com/TrebledJ" },
 			{ name: "StackOverflow", icon: "fab fa-stack-overflow", color: 'rgb(236, 124, 34)', link: "https://stackoverflow.com/users/10239789/trebledj" },
-			{ name: "CodinGame", img: "/img/logos/codingame.png", link: "https://www.codingame.com/profile/8444100ecb9723c1ec542346b0630aaa2821532" },
+			// { name: "CodinGame", img: "/img/logos/codingame.png", link: "https://www.codingame.com/profile/8444100ecb9723c1ec542346b0630aaa2821532" },
 			{ name: "SoundCloud", icon: "fab fa-soundcloud", color: 'rgb(237, 110, 30)', link: "https://soundcloud.com/trebledj" },
 			{ name: "MuseScore", img: "/img/logos/musescore.png", link: "https://musescore.com/user/20636901" },
 			// {name: "Spotify", icon: "fab fa-spotify", link: "https://open.spotify.com/user/24i9exez29k1jr85olljuy94g?si=4213a229649f4ceb"},
