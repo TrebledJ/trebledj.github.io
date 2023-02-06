4a78fcfa1266a4e98981eb0dc75a9e3365a42704
Johnathan <jjjlaw@connect.ust.hk>
2023-02-07 00:08:19 +0800

    add highlight to toc section

--- assets/js/head.js
+++ assets/js/head.js
@@ -34,11 +34,3 @@ $(function () {
         document.documentElement.scrollTop = 0;
     });
 });
-
-const copyUrlToClipboard = async () => {
-    try {
-        await navigator.clipboard.writeText(location.href);
-    } catch (err) {
-        console.error("Failed to copy: ", err);
-    }
-};
---
+++ assets/js/post.js
@@ -0,0 +1,62 @@
+const copyUrlToClipboard = async () => {
+    try {
+        await navigator.clipboard.writeText(location.href);
+    } catch (err) {
+        console.error("Failed to copy: ", err);
+    }
+};
+
+
+$(function () {
+    const headerOffset = 75;
+    const sections = document.querySelectorAll(".post-article h2,.post-article h3");
+    const menu = document.querySelectorAll("nav.toc a");
+
+    const hash = window.location.hash;
+
+    if (hash) {
+        for (const item of menu) {
+            if (menu.href === hash) {
+                item.classList.add("active");
+            }
+        }
+    }
+
+    const makeActive = (link) => {
+        if (menu[link]) {
+            menu[link].classList.add("active");
+        }
+    };
+    const removeActive = (link) => menu[link].classList.remove("active");
+    const removeAllActive = () =>
+        [...Array(sections.length).keys()].forEach((link) =>
+            removeActive(link)
+        );
+
+    let currentActive = 0;
+
+    const updateTOCHighlight = () => {
+        {
+            var docElem = document.documentElement;
+            var docBody = document.body;
+            var scrollTop = (docBody.scrollTop || docElem.scrollTop);
+
+            const currentHeading =
+                sections.length -
+                [...sections].reverse().findIndex((section) => {
+                    return section.offsetTop - headerOffset <= scrollTop;
+                }) -
+                1;
+
+            if (currentHeading !== currentActive) {
+                removeAllActive();
+                currentActive = currentHeading;
+                makeActive(currentHeading);
+            }
+        }
+    };
+
+
+    $(window).on("scroll", updateTOCHighlight);
+    updateTOCHighlight();
+})
