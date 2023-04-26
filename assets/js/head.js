1f708311f0c0185008127041895c11a209530328
TrebledJ <39648915+TrebledJ@users.noreply.github.com>
2023-04-26 10:36:32 +0800

    quick 'n dirty bugfix 3(?)

--- assets/js/head.js
+++ assets/js/head.js
@@ -18,4 +18,38 @@ $(function () {
         document.body.scrollTop = 0;
         document.documentElement.scrollTop = 0;
     });
+
+    // Nanobar.
+    var options = {
+        classname: 'load-progress-bar',
+        id: 'my-id'
+    };
+    var nanobar = new Nanobar(options);
+    nanobar.go(30);
+    nanobar.go(76);
+    nanobar.go(100);
+
+    // Load tooltips.
+    $("body").tooltip({ selector: '[data-toggle=tooltip]' });
+
+    // /**
+    //  * @param {String} HTML representing a single element
+    //  * @return {Element}
+    //  */
+    // function htmlToElement(html) {
+    //     var template = document.createElement('template');
+    //     html = html.trim(); // Never return a text node of whitespace as the result
+    //     template.innerHTML = html;
+    //     return template.content.firstChild;
+    // }
+
+    // /**
+    //  * @param {String} HTML representing any number of sibling elements
+    //  * @return {NodeList} 
+    //  */
+    // function htmlToElements(html) {
+    //     var template = document.createElement('template');
+    //     template.innerHTML = html;
+    //     return template.content.childNodes;
+    // }
 });
