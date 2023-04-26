504d659f70ab4aacfa51b9facc9a5f252353e981
TrebledJ <39648915+TrebledJ@users.noreply.github.com>
2023-04-26 13:01:15 +0800

    update feeds, metadata

--- _data/feedtags.js
+++ _data/feedtags.js
@@ -1,9 +1,14 @@
+// List the tags to generate feeds for.
 module.exports = [
     'posts',
     'programming',
+    'music',
     'composition',
     'electronic',
     'ctf',
     'project',
+    'mathematics',
     'tutorial',
+    'experience',
+    'faith',
 ];
