b3c7d5c24605e4a9654a97405fc3703e2581e1d3
TrebledJ <39648915+TrebledJ@users.noreply.github.com>
2023-04-26 11:16:38 +0800

    nvm, revert theme-switch refactor in favour of faster paint

--- assets/js/head.js
+++ assets/js/head.js
@@ -39,7 +39,6 @@ $(function () {
     /* Vars */
     var base_url = '{{site.baseurl}}';
     var soundcloud_color = '{{site.soundcloud_color}}';
-    var defaultTheme = "dark";
 
     /* Nanobar */
     var options = {
@@ -53,46 +52,4 @@ $(function () {
 
     /* Load Tooltips */
     $("body").tooltip({ selector: '[data-toggle=tooltip]' });
-
-    /* Theme */
-    function modeInit() {
-        let currentTheme = localStorage.getItem("theme");
-        if (currentTheme === "dark") {
-            setMode("dark");
-        } else if (currentTheme === "light") {
-            setMode("light");
-        } else {
-            setMode(defaultTheme);
-        }
-
-        // Set switch to correct setting on load.
-        document.getElementById('theme-toggle').checked = (currentTheme === "light");
-    }
-
-    function modeSwitcher() {
-        let currentTheme = localStorage.getItem("theme");
-        if (currentTheme === "dark") {
-            setMode("light");
-            if (reloadDisqus)
-                reloadDisqus();
-        } else if (currentTheme === "light") {
-            setMode("dark");
-            if (reloadDisqus)
-                reloadDisqus();
-        } else {
-            setMode(defaultTheme);
-        }
-    }
-
-    function setMode(mode) {
-        if (mode === "dark") {
-            document.documentElement.setAttribute("data-theme", "dark");
-            localStorage.setItem("theme", "dark");
-        } else if (mode === "light") {
-            document.documentElement.setAttribute("data-theme", "light");
-            localStorage.setItem("theme", "light");
-        }
-    }
-
-    modeInit();
 });
---
+++ assets/js/theme-switcher.js
@@ -0,0 +1,42 @@
+/* Theme */
+var defaultTheme = "dark";
+
+function modeInit() {
+    let currentTheme = localStorage.getItem("theme");
+    if (currentTheme === "dark") {
+        setMode("dark");
+    } else if (currentTheme === "light") {
+        setMode("light");
+    } else {
+        setMode(defaultTheme);
+    }
+
+    // Set switch to correct setting on load.
+    document.getElementById('theme-toggle').checked = (currentTheme === "light");
+}
+
+function modeSwitcher() {
+    let currentTheme = localStorage.getItem("theme");
+    if (currentTheme === "dark") {
+        setMode("light");
+        if (reloadDisqus)
+            reloadDisqus();
+    } else if (currentTheme === "light") {
+        setMode("dark");
+        if (reloadDisqus)
+            reloadDisqus();
+    } else {
+        setMode(defaultTheme);
+    }
+}
+
+function setMode(mode) {
+    if (mode === "dark") {
+        document.documentElement.setAttribute("data-theme", "dark");
+        localStorage.setItem("theme", "dark");
+    } else if (mode === "light") {
+        document.documentElement.setAttribute("data-theme", "light");
+        localStorage.setItem("theme", "light");
+    }
+
+}
