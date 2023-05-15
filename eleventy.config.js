56aedf09800b94e1b3846484e54a1a7f4a74a15c
TrebledJ <39648915+TrebledJ@users.noreply.github.com>
2023-05-16 04:50:40 +0800

    chore: remove redundance in eleventy.config

--- eleventy.config.js
+++ eleventy.config.js
@@ -27,29 +27,9 @@ module.exports = function (eleventyConfig) {
 	// https://www.11ty.dev/docs/watch-serve/#add-your-own-watch-targets
 
 	// Process content images to the image pipeline.
-	eleventyConfig.addWatchTarget("assets/**/*.{png,jpeg}");
+	eleventyConfig.addWatchTarget("assets/**/*.{png,jpg}");
 
-	// eleventyConfig.setBrowserSyncConfig(
-	// 	require('./browsersync')('_site')
-	// );
-	eleventyConfig.setBrowserSyncConfig({
-		callbacks: {
-			ready: function (err, bs) {
-				bs.addMiddleware("*", (req, res) => {
-					if (!fs.existsSync(NOT_FOUND_PATH)) {
-						throw new Error(`Expected a \`${NOT_FOUND_PATH}\` file but could not find one. Did you create a 404.html template?`);
-					}
-
-					const content_404 = fs.readFileSync(NOT_FOUND_PATH);
-					// Add 404 http status code in request header.
-					res.writeHead(404, { "Content-Type": "text/html; charset=UTF-8" });
-					// Provides the 404 content without redirect.
-					res.write(content_404);
-					res.end();
-				});
-			}
-		}
-	});
+	eleventyConfig.setWatchThrottleWaitTime(100);
 
 	eleventyConfig.setServerOptions({
 		liveReload: true,
