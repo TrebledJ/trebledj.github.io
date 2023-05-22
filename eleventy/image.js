77fbfae8afe747eebc0a4463698381ea751bf1c9
TrebledJ <39648915+TrebledJ@users.noreply.github.com>
2023-05-23 03:51:32 +0800

    chore: format and comment

--- eleventy/image.js
+++ eleventy/image.js
@@ -3,84 +3,85 @@ const eleventyImage = require("@11ty/eleventy-img");
 
 
 module.exports = function (eleventyConfig) {
-	function relativeToInputPath(inputPath, relativeFilePath) {
-		let split = inputPath.split("/");
-		split.pop();
+    function relativeToInputPath(inputPath, relativeFilePath) {
+        let split = inputPath.split("/");
+        split.pop();
 
-		return path.resolve(split.join(path.sep), relativeFilePath);
-	}
+        return path.resolve(split.join(path.sep), relativeFilePath);
+    }
 
     // Get settings and options.
     function getOptions(src) {
         const extDefault = ['gif'].find(ext => src.endsWith(ext)); // Use the extension special? Then use it.
-		const ext = extDefault || 'webp';
-		const animated = src.endsWith('gif');
+        const ext = extDefault || 'webp';
+        const animated = src.endsWith('gif');
 
-		// Full list of formats here: https://www.11ty.dev/docs/plugins/image/#output-formats
-		const formats = [ext];
-		// let file = relativeToInputPath(this.page.inputPath, src);
-		const file = src;
-		const options = {
-			widths: ["auto"],
-			formats,
-			outputDir: path.join(eleventyConfig.dir.output, "img"),
-			sharpOptions: {
-				animated,
-			},
-		};
-        return {ext, file, options};
+        // Full list of formats here: https://www.11ty.dev/docs/plugins/image/#output-formats
+        const formats = [ext];
+        // let file = relativeToInputPath(this.page.inputPath, src);
+        const file = src;
+        const options = {
+            widths: ["auto"],
+            formats,
+            outputDir: path.join(eleventyConfig.dir.output, "img"),
+            sharpOptions: {
+                animated,
+            },
+        };
+        return { ext, file, options };
     }
 
+    // Modify classes with custom processing, returns an array of classes.
     function amendClasses(classes) {
         classes = (typeof classes === 'string' ? classes.split(' ') : typeof classes === 'undefined' ? [] : classes);
-		classes.reverse(); // Add classes to the front.
-		if (classes.includes('post1')) {
-			// Remove class.
-			classes.splice(classes.indexOf('post1'), 1);
+        classes.reverse(); // Add classes to the front.
+        if (classes.includes('post1')) {
+            // Remove class.
+            classes.splice(classes.indexOf('post1'), 1);
 
-			if (classes.every(c => !c.startsWith('w-')))
-				classes.push('w-100'); // Default to full-width;
+            if (classes.every(c => !c.startsWith('w-')))
+                classes.push('w-100'); // Default to full-width;
 
-			// Solo image.
-			classes.push('center');
-			classes.push('rw'); 	// Responsive-width for small screens.
-			classes.push('mb-2'); 	// Extra spacing in the bottom.
-		}
-		classes.reverse();
+            // Solo image.
+            classes.push('center');
+            classes.push('rw'); 	// Responsive-width for small screens.
+            classes.push('mb-2'); 	// Extra spacing in the bottom.
+        }
+        classes.reverse();
         return classes;
     }
 
 
     async function imageAsyncShortcode(src, altText, classes) {
-		const {ext, file, options} = getOptions(src);
-		const metadata = await eleventyImage(file, options);
+        const { ext, file, options } = getOptions(src);
+        const metadata = await eleventyImage(file, options);
 
         classes = amendClasses(classes);
 
-		const data = metadata[ext][metadata[ext].length - 1];
-		const ratio = `aspect-ratio: auto ${data.width} / ${data.height};`; // Alleviate content layout shift.
+        const data = metadata[ext][metadata[ext].length - 1];
+        const ratio = `aspect-ratio: auto ${data.width} / ${data.height};`; // Alleviate content layout shift.
 
-		return `<img src="${data.url}" class="${classes.join(' ')}" alt="${altText}" title="${altText}" style="${ratio}" loading="lazy" decoding="async">`;
-	}
+        return `<img src="${data.url}" class="${classes.join(' ')}" alt="${altText}" title="${altText}" style="${ratio}" loading="lazy" decoding="async">`;
+    }
 
     function imageSyncShortcode(src, altText, classes) {
-		const {ext, file, options} = getOptions(src);
-		eleventyImage(file, options);
+        const { ext, file, options } = getOptions(src);
+        eleventyImage(file, options);
 
         classes = amendClasses(classes);
 
         const metadata = eleventyImage.statsSync(file, options);
-		const data = metadata[ext][metadata[ext].length - 1];
-		const ratio = `aspect-ratio: auto ${data.width} / ${data.height};`; // Alleviate content layout shift.
+        const data = metadata[ext][metadata[ext].length - 1];
+        const ratio = `aspect-ratio: auto ${data.width} / ${data.height};`; // Alleviate content layout shift.
 
-		return `<img src="${data.url}" class="${classes.join(' ')}" alt="${altText}" title="${altText}" style="${ratio}" loading="lazy" decoding="async">`;
-	}
+        return `<img src="${data.url}" class="${classes.join(' ')}" alt="${altText}" title="${altText}" style="${ratio}" loading="lazy" decoding="async">`;
+    }
 
-	// Eleventy Image shortcode
-	// https://www.11ty.dev/docs/plugins/image/
-	eleventyConfig.addAsyncShortcode("image", imageAsyncShortcode);
+    // Eleventy Image shortcode
+    // https://www.11ty.dev/docs/plugins/image/
+    eleventyConfig.addAsyncShortcode("image", imageAsyncShortcode);
 
-	// Synchronous shortcode. Useful for Nunjucks macro.
-	// Doesn't work with remote URLs.
-	eleventyConfig.addShortcode("imageSync", imageSyncShortcode);
+    // Synchronous shortcode. Useful for Nunjucks macro.
+    // Doesn't work with remote URLs.
+    eleventyConfig.addShortcode("imageSync", imageSyncShortcode);
 };
