39c85fbe366b47796502986b0f68554e2dcff672
TrebledJ <39648915+TrebledJ@users.noreply.github.com>
2023-05-25 01:46:28 +0800

    chore: compress icons

--- _data/site.js
+++ _data/site.js
@@ -41,9 +41,9 @@ Feel free to [reach out](#contact) for whatever reason. ヽ(・∀・)ﾉ
 		social: [
 			{ name: "GitHub", icon: "fab fa-github", color: 'rgb(150, 60, 180)', link: "http://github.com/TrebledJ" },
 			{ name: "StackOverflow", icon: "fab fa-stack-overflow", color: 'rgb(236, 124, 34)', link: "https://stackoverflow.com/users/10239789/trebledj" },
-			{ name: "CodinGame", img: "/img/logos/codingame.png", link: "https://www.codingame.com/profile/8444100ecb9723c1ec542346b0630aaa2821532" },
+			{ name: "CodinGame", img: "/img/logos/codingame.webp", link: "https://www.codingame.com/profile/8444100ecb9723c1ec542346b0630aaa2821532" },
 			{ name: "SoundCloud", icon: "fab fa-soundcloud", color: 'rgb(237, 110, 30)', link: "https://soundcloud.com/trebledj" },
-			{ name: "MuseScore", img: "/img/logos/musescore.png", link: "https://musescore.com/user/20636901" },
+			{ name: "MuseScore", img: "/img/logos/musescore.webp", link: "https://musescore.com/user/20636901" },
 			// {name: "Spotify", icon: "fab fa-spotify", link: "https://open.spotify.com/user/24i9exez29k1jr85olljuy94g?si=4213a229649f4ceb"},
 			{ name: "Discord (TrebledJ#7595)", icon: "fab fa-discord", color: 'rgb(84, 100, 235)', link: `https://discordapp.com/users/220427982798454794` },
 			{ name: "Mail", icon: "fas fa-envelope", link: `mailto:${email}` },
