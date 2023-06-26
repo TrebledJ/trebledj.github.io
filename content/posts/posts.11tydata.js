af08edbbd4d985958701a4160a148908628738de
TrebledJ <39648915+TrebledJ@users.noreply.github.com>
2023-06-26 15:01:24 +0800

    chore: rename vars

--- content/posts/posts.11tydata.js
+++ content/posts/posts.11tydata.js
@@ -4,10 +4,10 @@ const path = require("path");
 const spawn = require("cross-spawn");
 
 function getGitCommitDateFromPathByMessage(filePath, regex) {
-	let logs;
+	let output;
 
 	try {
-		logs = spawn.sync(
+		output = spawn.sync(
 			"git",
 			["log", "--follow", "--format=%at %s", path.basename(filePath)],
 			{ cwd: path.dirname(filePath) }
@@ -16,15 +16,15 @@ function getGitCommitDateFromPathByMessage(filePath, regex) {
 		throw new Error("Fail to run 'git log'");
 	}
 
-	if (logs && logs.stdout) {
-		const commits = logs.stdout.toString("utf-8").split('\n');
+	if (output && output.stdout) {
+		const commits = output.stdout.toString("utf-8").split('\n');
 
 		// Filter commits which match the regex.
-		const outputs = !regex ? commits : commits.filter(s => s.substring(s.indexOf(' ') + 1).match(regex));
+		const filtered = !regex ? commits : commits.filter(s => s.substring(s.indexOf(' ') + 1).match(regex));
 
-		if (outputs && outputs[0]) {
+		if (filtered && filtered[0]) {
 			// Grab latest commit timestamp.
-			const s = outputs[0];
+			const s = filtered[0];
 			const ts = parseInt(s.substring(0, s.indexOf(' ')), 10) * 1000;
 
 			// Paths not commited to Git returns empty timestamps, resulting in NaN.
