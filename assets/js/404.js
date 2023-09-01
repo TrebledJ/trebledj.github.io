a9f2469bc2f93a4908e9315c59d976067b782a48
TrebledJ <39648915+TrebledJ@users.noreply.github.com>
2023-09-01 14:04:09 +0800

    fix: load 404 meme

--- assets/js/404.js
+++ assets/js/404.js
@@ -2,6 +2,6 @@
 $(function () {
     const numMemes = 4;
     let img = Math.floor(Math.random() * numMemes) + 1;
-    $(".404-meme").html(`<img src="/img/posts/memes/404-${img}.jpg" class="w-75">`);
+    $(".404-meme").html(`<img src="/img/memes/404-${img}.jpg" class="w-75">`);
     // $(".post-posted-date").text('Posted on ' + new Date().toISOString().split('T')[0]);
 });
