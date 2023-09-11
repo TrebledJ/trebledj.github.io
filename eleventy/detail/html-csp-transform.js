f0ef2597ab4bb1427cbc27edee9208bd7e677add
TrebledJ <39648915+TrebledJ@users.noreply.github.com>
2023-09-12 02:52:38 +0800

    chore: cleanup csp transform thingy

--- eleventy/detail/html-csp-transform.js
+++ eleventy/detail/html-csp-transform.js
@@ -1,4 +1,4 @@
-const collectHashes = require('./html-csp-transform-collect');
+const collectHashes = require('./html-csp-collect-hashes');
 
 module.exports = function (content) {
     if (this.page.outputPath && this.page.outputPath.endsWith(".html")) {
@@ -6,7 +6,7 @@ module.exports = function (content) {
 
         return content
             .replace(/\bscript-src (.*?'self')/, `script-src $1 ${Array.from(new Set(hashes.script)).join(' ')}`)
-            .replace(/\bstyle-src (.*?'self')/, `style-src $1 ${Array.from(new Set(hashes.style)).join(' ')}`);
+            // .replace(/\bstyle-src (.*?'self')/, `style-src $1 ${Array.from(new Set(hashes.style)).join(' ')}`)
     }
     return content;
 }
