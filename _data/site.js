974ca587e64e674d4744317dedd8978d625992d8
TrebledJ <39648915+TrebledJ@users.noreply.github.com>
2023-10-06 01:16:58 +0800

    chore: cleanup varnames and site config

--- _data/site.js
+++ _data/site.js
@@ -2,19 +2,6 @@ const csp = require('./csp');
 
 module.exports = function () {
 	const email = "trebledjjj@gmail.com"
-	const bio = `
-Hi! (｡＾ ᴗ＾)ﾉ
-I'm a recent graduate of the [Hong Kong University of Science and Technology (HKUST)](http://hkust.edu.hk/).
-I'm passionate in a wide range of fields, from [software engineering](/tags/software-engineering) to [music](/tags/music).
-Software engineering is such a vast field.
-There's the joy and pain of [app development](/tags/apps), the tension and fun in [cybersecurity](/tags/ctf), and the tangible excitement of [robotics](/tags/robotics).
-And let's not forget the gritty (yet beautiful) backbone of [programming](/tags/programming).
-
-More about me? I like to solve problems, learn new things, and pass it on through teaching/mentoring.
-In my spare time, I enjoy taking walks, [composing music](/tags/composition), play the occasional CTF, and putting my thoughts down on ~~paper~~ text.
-
-Feel free to [reach out](#contact) for whatever reason. ヽ(・∀・)ﾉ
-`.trim();
 
 	return {
 		title: "TrebledJ's Pages",
@@ -23,12 +10,25 @@ Feel free to [reach out](#contact) for whatever reason. ヽ(・∀・)ﾉ
 		baseurl: "",
 		language: "en",
 		description: "TrebledJ's personal blog on programming, music, and memes.",
+
 		author: {
 			alias: "TrebledJ",
 			name: "Johnathan",
 			email: email,
 			logo: "profile-icon.jpg",
-			bio: bio,
+			bio: `
+			Hi! (｡＾ ᴗ＾)ﾉ
+			I'm a recent graduate of the [Hong Kong University of Science and Technology (HKUST)](http://hkust.edu.hk/).
+			I'm passionate in a wide range of fields, from [software engineering](/tags/software-engineering) to [music](/tags/music).
+			Software engineering is such a vast field.
+			There's the joy and pain of [app development](/tags/apps), the tension and fun in [cybersecurity](/tags/ctf), and the tangible excitement of [robotics](/tags/robotics).
+			And let's not forget the gritty (yet beautiful) backbone of [programming](/tags/programming).
+			
+			More about me? I like to solve problems, learn new things, and pass it on through teaching/mentoring.
+			In my spare time, I enjoy taking walks, [composing music](/tags/composition), play the occasional CTF, and putting my thoughts down to ~~paper~~ text.
+			
+			Feel free to [reach out](#contact) for whatever reason. ヽ(・∀・)ﾉ
+			`.trim().replace(/^[ \t]+/gm, ''),
 		},
 		navpages: [
 			{ text: 'Home', url: '/' },
@@ -48,14 +48,17 @@ Feel free to [reach out](#contact) for whatever reason. ヽ(・∀・)ﾉ
 			],
 			resultDefaultIcon: "book",
 		},
-		github_username: "TrebledJ",
-		musescore_id: "20636901",
-		// google_analytics: "G-7TQ4WV0LMK",
-		cloudflare_analytics: "0c1f01f6fd2340a9a7c7abb8072e3857",
-		disqus_shortname: "trebledj",
-		soundcloud_color: "2631c8",
-		getform_endpoint: "3b2a26ea-31c4-49ef-a496-d4d765773e59",
-		hcaptcha_sitekey: "85d67539-c1f8-4bed-b164-17c3b4528972",
+		analytics: {
+			// google: "G-7TQ4WV0LMK",
+			cloudflare: "0c1f01f6fd2340a9a7c7abb8072e3857",
+		},
+		id: {
+			musescore: "20636901",
+			disqus: "trebledj",
+			getform: "3b2a26ea-31c4-49ef-a496-d4d765773e59",
+			hcaptcha: "85d67539-c1f8-4bed-b164-17c3b4528972",
+		},
+		soundcloudColor: "2631c8",
 		social: [
 			{ name: "GitHub", icon: "fab fa-github", color: 'rgb(150, 60, 180)', link: "https://github.com/TrebledJ" },
 			{ name: "StackOverflow", icon: "fab fa-stack-overflow", color: 'rgb(236, 124, 34)', link: "https://stackoverflow.com/users/10239789/trebledj" },
