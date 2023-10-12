b05c3980e92d5be743aa0f51f66d5499de3bb6dc
TrebledJ <39648915+TrebledJ@users.noreply.github.com>
2023-10-13 00:22:28 +0800

    chore: cleanup

--- _data/site.js
+++ _data/site.js
@@ -21,11 +21,11 @@ module.exports = function () {
 			I'm a recent graduate of the [Hong Kong University of Science and Technology (HKUST)](http://hkust.edu.hk/).
 			I'm passionate in a wide range of fields, from [software engineering](/tags/software-engineering) to [music](/tags/music).
 			Software engineering is such a vast field.
-			There's the joy and pain of [app development](/tags/apps), the tension and fun in [cybersecurity](/tags/ctf), and the tangible excitement of [robotics](/tags/robotics).
-			And let's not forget the gritty (yet beautiful) backbone of [programming](/tags/programming).
+			There's almost an endless thrill in building robust [apps](/tags/apps), exploring the deep field of [cybersecurity](/tags/ctf), and the toying with [robots](/tags/robotics) and circuitry.
+			And let's not forget the gritty (yet beautiful) art of [programming](/tags/programming)—simply enjoying [languages](/tags/programming-languages) for their succinctness or ability to express complex thoughts.
 			
-			More about me? I like to solve problems, learn new things, and pass it on through teaching/mentoring.
-			In my spare time, I enjoy taking walks, [composing music](/tags/composition), play the occasional CTF, and putting my thoughts down to ~~paper~~ text.
+			More about me? I like to solve problems, learn new things, and pass it on through teaching.
+			In my spare time, I enjoy taking walks, [composing music](/tags/composition), playing the occasional CTF, and putting my thoughts down to ~~paper~~ text.
 			
 			Feel free to [reach out](#contact) for whatever reason. ヽ(・∀・)ﾉ
 			`.trim().replace(/^[ \t]+/gm, ''),
@@ -49,16 +49,20 @@ module.exports = function () {
 			resultDefaultIcon: "book",
 		},
 		analytics: {
+			// Pick your poison.
 			// google: "G-7TQ4WV0LMK",
 			cloudflare: "0c1f01f6fd2340a9a7c7abb8072e3857",
 		},
 		id: {
+			// Unique identifiers and instances.
 			musescore: "20636901",
 			disqus: "trebledj",
 			getform: "3b2a26ea-31c4-49ef-a496-d4d765773e59",
 			hcaptcha: "85d67539-c1f8-4bed-b164-17c3b4528972",
 		},
-		soundcloudColor: "2631c8",
+		color: {
+			soundcloud: "2631c8",
+		},
 		social: [
 			{ name: "GitHub", icon: "fab fa-github", color: 'rgb(150, 60, 180)', link: "https://github.com/TrebledJ" },
 			{ name: "StackOverflow", icon: "fab fa-stack-overflow", color: 'rgb(236, 124, 34)', link: "https://stackoverflow.com/users/10239789/trebledj" },
