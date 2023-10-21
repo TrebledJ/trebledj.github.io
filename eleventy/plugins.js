baa695ca3d47971cd095d87637706eaadee8e911
TrebledJ <39648915+TrebledJ@users.noreply.github.com>
2023-10-21 23:24:43 +0800

    lint: fix stuff

--- eleventy/plugins.js
+++ eleventy/plugins.js
@@ -1,59 +1,42 @@
-const path = require("path");
-
-const pluginRss = require("@11ty/eleventy-plugin-rss");
-const pluginSyntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
-const pluginNavigation = require("@11ty/eleventy-navigation");
-const { EleventyHtmlBasePlugin, EleventyRenderPlugin } = require("@11ty/eleventy");
-const eleventySass = require("eleventy-sass");
-
+const pluginRss = require('@11ty/eleventy-plugin-rss');
+const pluginSyntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight');
+const pluginNavigation = require('@11ty/eleventy-navigation');
+const { EleventyHtmlBasePlugin, EleventyRenderPlugin } = require('@11ty/eleventy');
+const eleventySass = require('eleventy-sass');
 
 module.exports = function (eleventyConfig) {
-	eleventyConfig.addPlugin(pluginRss);
-	eleventyConfig.addPlugin(pluginSyntaxHighlight, {
-		errorOnInvalidLanguage: true
-	});
-	eleventyConfig.addPlugin(pluginNavigation);
-	eleventyConfig.addPlugin(EleventyHtmlBasePlugin);
-	eleventyConfig.addPlugin(EleventyRenderPlugin);
+  eleventyConfig.addPlugin(pluginRss);
+  eleventyConfig.addPlugin(pluginSyntaxHighlight, {
+    errorOnInvalidLanguage: true,
+  });
+  eleventyConfig.addPlugin(pluginNavigation);
+  eleventyConfig.addPlugin(EleventyHtmlBasePlugin);
+  eleventyConfig.addPlugin(EleventyRenderPlugin);
 
-	eleventyConfig.addPlugin(eleventySass/* , {
-		compileOptions: {
-			permalink: function (contents, inputPath) {
-				return (data) => data.page.filePathStem.replace(/^\/scss\//, "/css/") + ".css";
-			}
-		}
-	} */);
+  eleventyConfig.addPlugin(eleventySass);
 
-	function relativeToInputPath(inputPath, relativeFilePath) {
-		let split = inputPath.split("/");
-		split.pop();
+  eleventyConfig.addPlugin(eleventyConfig => {
+    eleventyConfig.on('eleventy.before', () => {
+      process.env.ENABLE_LIGHTBOX = true;
+    });
+  });
 
-		return path.resolve(split.join(path.sep), relativeFilePath);
-	}
+  // Drafts implementation, see `content/content.11tydata.js` for additional code.
+  // This section *could* be simplified to an environment variable in an npm script,
+  // but this way an ENV is not required and this code works cross-platform.
+  eleventyConfig.addPlugin(eleventyConfig => {
+    let logged = false;
+    eleventyConfig.on('eleventy.before', ({ runMode }) => {
+      // Only show drafts in serve/watch modes
+      if (runMode === 'serve' || runMode === 'watch') {
+        process.env.BUILD_DRAFTS = true;
 
-	eleventyConfig.addPlugin(function enableLightbox(eleventyConfig) {
-		eleventyConfig.on("eleventy.before", () => {
-			process.env.ENABLE_LIGHTBOX = true;
-		});
-	});
+        // Only log once.
+        if (!logged)
+          console.log('[11ty] including `draft: true` posts');
 
-	// Drafts implementation, see `content/content.11tydata.js` for additional code.
-	// This section *could* be simplified to an environment variable in an npm script,
-	// but this way an ENV is not required and this code works cross-platform.
-	eleventyConfig.addPlugin(function enableDrafts(eleventyConfig) {
-		let logged = false;
-		eleventyConfig.on("eleventy.before", ({ runMode }) => {
-			// Only show drafts in serve/watch modes
-			if (runMode === "serve" || runMode === "watch") {
-				process.env.BUILD_DRAFTS = true;
-
-				// Only log once.
-				if (!logged) {
-					console.log("[11ty] including `draft: true` posts");
-				}
-
-				logged = true;
-			}
-		});
-	})
+        logged = true;
+      }
+    });
+  });
 };
--- eleventy/shortcodes.js
+++ eleventy/shortcodes.js
@@ -4,77 +4,69 @@
 
 const image = require('./image');
 
-
 module.exports = function (eleventyConfig) {
-	image(eleventyConfig);
+  image(eleventyConfig);
 
+  eleventyConfig.addShortcode('alert', (role, icon) => {
+    const alert = {
+      info: ['info', 'circle-info'],
+      fact: ['info', 'bolt'],
+      warning: ['warning', 'triangle-exclamation'],
+      success: ['success', 'lightbulb'],
+      danger: ['danger', 'radiation'],
+      simple: ['secondary', ''],
+    };
+    const accepted = Object.keys(alert);
+    if (!accepted.includes(role))
+      throw new Error(`Expected a valid alert role (${accepted.join(', ')}), but got ${role}.`);
 
-	eleventyConfig.addShortcode("alert", function (role, emoji) {
-		const alert = {
-			info: ['info', 'circle-info'],
-			fact: ['info', 'bolt'],
-			warning: ['warning', 'triangle-exclamation'],
-			success: ['success', 'lightbulb'],
-			danger: ['danger', 'radiation'],
-			simple: ['secondary', ''],
-		};
-		const accepted = Object.keys(alert);
-		if (!accepted.includes(role)) {
-			throw new Error(`Expected a valid alert role (${accepted.join(', ')}), but got ${role}.`);
-		}
+    const [state, defaultEmoji] = alert[role];
+    if (!icon)
+      icon = defaultEmoji;
 
-		const [state, defaultEmoji] = alert[role];
-		if (!emoji) {
-			emoji = defaultEmoji;
-		}
-		let emoji_line = '';
-		if (emoji) {
-			emoji_line = `<i class="fa fa-${emoji} ms-1 me-3 mt-1 fs-4" role="img"></i>`;
-		}
+    let iconHtml = '';
+    if (icon)
+      iconHtml = `<i class="fa fa-${icon} ms-1 me-3 mt-1 fs-4" role="img"></i>`;
 
-		return `<div class="alert alert-${state} d-flex align-items-start">${emoji_line}<div class="alert-content flex-fill mt-0">\n`;
-	});
+    return `<div class="alert alert-${state} d-flex align-items-start">
+              ${iconHtml}
+              <div class="alert-content flex-fill mt-0">\n`.replace(/\s{2,}/g, ' ');
+  });
 
-	// Paired shortcode workaround: see Note [endalert Workaround].
-	eleventyConfig.addShortcode("endalert", function () {
-		return `</div></div>`;
-	});
+  // Paired shortcode workaround: see Note [endalert Workaround].
+  eleventyConfig.addShortcode('endalert', () => '</div></div>');
 
+  eleventyConfig.addShortcode('details', (summary, state) => {
+    state ||= '';
+    const md = eleventyConfig.getFilter('mdInline');
+    return `<details ${state}><summary>${md(summary)}</summary><div class="details-content">\n`;
+  });
 
-	eleventyConfig.addShortcode("details", function (summary, state) {
-		state ||= '';
-		const md = eleventyConfig.getFilter("mdInline");
-		return `<details ${state}><summary>${md(summary)}</summary><div class="details-content">\n`;
-	});
+  // Paired shortcode workaround: see Note [endalert Workaround].
+  eleventyConfig.addShortcode('enddetails', () => (
+    '<div class="details-collapse-bottom"><sub><a>(collapse)</a></sub></div></div></details>'
+  ));
 
-	// Paired shortcode workaround: see Note [endalert Workaround].
-	eleventyConfig.addShortcode("enddetails", function () {
-		return `<div class="details-collapse-bottom"><sub><a>(collapse)</a></sub></div></div></details>`;
-	});
+  eleventyConfig.addShortcode('table', () => '<div class="table-container">');
 
-	
-	eleventyConfig.addShortcode("table", function () {
-		return `<div class="table-container">`;
-	});
+  // Paired shortcode workaround: see Note [endalert Workaround].
+  eleventyConfig.addShortcode('endtable', () => '</div>');
 
-	// Paired shortcode workaround: see Note [endalert Workaround].
-	eleventyConfig.addShortcode("endtable", function () {
-		return `</div>`;
-	});
+  const escapeHtml = unsafe => (
+    unsafe
+      .replaceAll('&', '&amp;').replaceAll('<', '&lt;').replaceAll('>', '&gt;')
+      .replaceAll('"', '&quot;')
+      .replaceAll("'", '&#039;')
+  );
 
-	
-	const escapeHtml = (unsafe) => {
-		return unsafe.replaceAll('&', '&amp;').replaceAll('<', '&lt;').replaceAll('>', '&gt;').replaceAll('"', '&quot;').replaceAll("'", '&#039;');
-	}
+  eleventyConfig.addShortcode('abbr', (term, expansion) => (
+    `<abbr data-placement="top" data-toggle="tooltip" title="${escapeHtml(expansion)}">${term}</abbr>`
+  ));
 
-	eleventyConfig.addShortcode("abbr", function (term, expansion) {
-		return `<abbr data-placement="top" data-toggle="tooltip" title="${escapeHtml(expansion)}">${term}</abbr>`;
-	});
-
-	eleventyConfig.addShortcode("tag", function (text, tag) {
-		tag ||= text;
-		return `<a class="tag" href="/tags/${tag}/">${text}</a>`
-	});
+  eleventyConfig.addShortcode('tag', (text, tag) => {
+    tag ||= text;
+    return `<a class="tag" href="/tags/${tag}/">${text}</a>`;
+  });
 };
 
 /**
