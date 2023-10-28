21ed15874d36ec5bbe5c970fa335c75c2496680f
TrebledJ <39648915+TrebledJ@users.noreply.github.com>
2023-10-28 21:17:24 +0800

    chore: remove comments

--- content/content.11tydata.js
+++ content/content.11tydata.js
@@ -1,3 +1,5 @@
+const site = require("../_data/site");
+
 module.exports = {
   showToc: false,
 
@@ -8,7 +10,7 @@ module.exports = {
     permalink: data => {
       let { permalink } = data;
       if (!permalink) {
-        if (data.page.filePathStem.startsWith('/posts') && data.page.outputFileExtension === 'html') {
+        if (data.page.filePathStem.startsWith(site.) && data.page.outputFileExtension === 'html') {
           // HTML Blog post.
           permalink = `/posts/${data.page.fileSlug}/index.html`;
         } else if (data.page.inputPath.endsWith('.js.njk')) {
