69d94464c34bf314cde1ba5081bc3222a595c99a
Lint Action <lint-action@samuelmeuli.com>
2023-11-08 17:31:06 +0000

    chore(lint): fix issues with ESLint
    [skip ci]

--- eleventy/detail/related.js
+++ eleventy/detail/related.js
@@ -105,7 +105,7 @@ function getRelatedPosts(posts, tags, related) {
   // Find relevant posts with same tags as `related.tags`.
   if (related.tags) {
     if (!Array.isArray(related.tags))
-      throw new Error("getRelatedPosts: expected array");
+      throw new Error('getRelatedPosts: expected array');
 
     for (const post of posts) {
       if (finalRelated.size >= numTargetPosts)
