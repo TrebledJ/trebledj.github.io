dee0dd2d7ea21beecbf688e531468961ea65e223
TrebledJ <39648915+TrebledJ@users.noreply.github.com>
2023-11-20 23:25:54 +0800

    fix: links user agent

--- scripts/links.sh
+++ scripts/links.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-lychee --base https://trebledj.me --no-progress --accept '200,201,202,203,204,403,429' --include-verbatim --exclude-mail --exclude-path _site/404.html _site
+lychee --base https://trebledj.me --user-agent 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36' --no-progress --accept '200,201,202,203,204,403,429' --include-verbatim --exclude-mail --exclude-path _site/404.html _site
