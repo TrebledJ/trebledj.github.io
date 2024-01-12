6fcb29038e476b0119e0cdcae5de86dcf25c1797
TrebledJ <39648915+TrebledJ@users.noreply.github.com>
2024-01-12 21:08:53 +0800

    ??? actions build doesn't work

--- eleventy.config.js
+++ eleventy.config.js
@@ -17,7 +17,7 @@ module.exports = function (eleventyConfig) {
   // Copy the contents of the `public` folder to the output folder
   // For example, `./public/css/` ends up in `_site/css/`
   eleventyConfig.addPassthroughCopy({
-    './assets/favicon.ico': '/',
+    './assets/favicon.ico': '/favicon.ico',
     './assets/img': '/img',
     './content/**/assets/*.mp4': '/img',
     './assets/webfonts': '/webfonts',
