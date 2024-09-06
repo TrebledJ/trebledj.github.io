ec0928bfa310b57cbed138655742216960667a0a
TrebledJ <39648915+TrebledJ@users.noreply.github.com>
2024-09-07 03:41:56 +0800

    chore: refactor 2/2 - move markdown fence renderer into its own file

---
+++ eleventy/detail/markdown-it/domify.js
@@ -0,0 +1,9 @@
+// Introduce browser APIs such as `document` and `window` to Node.
+// Make sure to call this prior to using `document`, e.g. before loading Prism plugins.
+
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+
+global.window = (new JSDOM('')).window;
+global.document = global.window.document;
+global.getComputedStyle = window.getComputedStyle;
