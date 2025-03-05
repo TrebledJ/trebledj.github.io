74933fcab3e46d846882488fbcd55f5a5a85d098
Lint Action <lint-action@samuelmeuli.com>
2025-03-05 18:08:18 +0000

    chore(lint): fix issues with ESLint
    [skip ci]

--- eleventy/detail/prism/prism-diff-highlight.js
+++ eleventy/detail/prism/prism-diff-highlight.js
@@ -1,142 +1,139 @@
 (function () {
+  if (typeof Prism === 'undefined') {
+    return;
+  }
 
-	if (typeof Prism === 'undefined') {
-		return;
-	}
+  const LANGUAGE_REGEX = /^diff-([\w-]+)/i;
+  const HTML_TAG = /<\/?(?!\d)[^\s>\/=$<%]+(?:\s(?:\s*[^\s>\/=]+(?:\s*=\s*(?:"[^"]*"|'[^']*'|[^\s'">=]+(?=[\s>]))|(?=[\s/>])))+)?\s*\/?>/g;
+  // this will match a line plus the line break while ignoring the line breaks HTML tags may contain.
+  const HTML_LINE = RegExp(/(?:__|[^\r\n<])*(?:\r\n?|\n|(?:__|[^\r\n<])(?![^\r\n]))/.source.replace(/__/g, function () { return HTML_TAG.source; }), 'gi');
 
+  let warningLogged = false;
 
-	var LANGUAGE_REGEX = /^diff-([\w-]+)/i;
-	var HTML_TAG = /<\/?(?!\d)[^\s>\/=$<%]+(?:\s(?:\s*[^\s>\/=]+(?:\s*=\s*(?:"[^"]*"|'[^']*'|[^\s'">=]+(?=[\s>]))|(?=[\s/>])))+)?\s*\/?>/g;
-	//this will match a line plus the line break while ignoring the line breaks HTML tags may contain.
-	var HTML_LINE = RegExp(/(?:__|[^\r\n<])*(?:\r\n?|\n|(?:__|[^\r\n<])(?![^\r\n]))/.source.replace(/__/g, function () { return HTML_TAG.source; }), 'gi');
+  const NEW_LINE_EXP = /\n(?!$)/g;
+  function iterLineParam(lines, lineNos, callback) {
+    if (!lineNos)
+      return;
+    // Iterates over a line parameter, e.g. 1-4,5,8-10, and runs callback(i) for each line.
+    const linesNum = lines.length;
 
-	var warningLogged = false;
+    for (const section of lineNos.split(',')) {
+      const range = section.split('-');
+      let lineStart = parseInt(range[0], 10);
+      let lineEnd = range.length === 2 ? parseInt(range[1], 10) : lineStart;
 
-	const NEW_LINE_EXP = /\n(?!$)/g;
-	function iterLineParam(lines, lineNos, callback) {
-		if (!lineNos) return;
-		// Iterates over a line parameter, e.g. 1-4,5,8-10, and runs callback(i) for each line.
-		const linesNum = lines.length;
+      if (!Number.isNaN(lineStart) && !Number.isNaN(lineEnd)) {
+        if (lineStart < 1) {
+          lineStart = 1;
+        }
+        if (lineEnd > linesNum) {
+          lineEnd = linesNum;
+        }
+        lineStart--; // 0-based index.
+        lineEnd--;
 
-		for (const section of lineNos.split(',')) {
-			const range = section.split('-');
-			let lineStart = parseInt(range[0], 10);
-			let lineEnd = range.length === 2 ? parseInt(range[1], 10) : lineStart;
+        for (let j = lineStart; j <= lineEnd; j++)
+          callback(j);
 
-			if (!Number.isNaN(lineStart) && !Number.isNaN(lineEnd)) {
-				if (lineStart < 1) {
-					lineStart = 1;
-				}
-				if (lineEnd > linesNum) {
-					lineEnd = linesNum;
-				}
-				lineStart--; // 0-based index.
-				lineEnd--;
+        if (lineEnd == linesNum - 1)
+          break;
+      }
+    }
+  }
 
-				for (let j = lineStart; j <= lineEnd; j++)
-					callback(j);
+  Prism.hooks.add('before-sanity-check', function (env) {
+    const lang = env.language;
+    const pre = env.element.parentElement;
+    if (pre.getAttribute('data-diff-add') || pre.getAttribute('data-diff-del')) {
+      const lines = env.code.split(NEW_LINE_EXP);
+      const added = new Set();
+      iterLineParam(lines, pre.getAttribute('data-diff-add'), i => {
+        lines[i] = `+${lines[i]}`;
+        added.add(i);
+      });
+      iterLineParam(lines, pre.getAttribute('data-diff-del'), i => {
+        if (!added.has(i)) {
+          lines[i] = `-${lines[i]}`;
+          added.add(i);
+        }
+      });
+      for (let i = 0; i < lines.length; i++) {
+        if (!added.has(i)) {
+          lines[i] = ` ${lines[i]}`;
+        }
+      }
+      env.code = lines.join('\n');
+    }
+    if (LANGUAGE_REGEX.test(lang) && !env.grammar) {
+      env.grammar = Prism.languages[lang] = Prism.languages.diff;
+    }
+  });
+  Prism.hooks.add('before-tokenize', function (env) {
+    if (!warningLogged && !Prism.languages.diff && !Prism.plugins.autoloader) {
+      warningLogged = true;
+      console.warn("Prism's Diff Highlight plugin requires the Diff language definition (prism-diff.js)."
+				+ "Make sure the language definition is loaded or use Prism's Autoloader plugin.");
+    }
 
-				if (lineEnd == linesNum - 1)
-					break;
-			}
-		}
-	}
+    const lang = env.language;
+    if (LANGUAGE_REGEX.test(lang) && !Prism.languages[lang]) {
+      Prism.languages[lang] = Prism.languages.diff;
+    }
+  });
 
+  Prism.hooks.add('wrap', function (env) {
+    let diffLanguage; let diffGrammar;
 
-	Prism.hooks.add('before-sanity-check', function (env) {
-		var lang = env.language;
-		var pre = env.element.parentElement;
-		if (pre.getAttribute('data-diff-add') || pre.getAttribute('data-diff-del')) {
-			var lines = env.code.split(NEW_LINE_EXP);
-			const added = new Set();
-			iterLineParam(lines, pre.getAttribute('data-diff-add'), (i) => {
-				lines[i] = '+' + lines[i];
-				added.add(i);
-			});
-			iterLineParam(lines, pre.getAttribute('data-diff-del'), (i) => {
-				if (!added.has(i)) {
-					lines[i] = '-' + lines[i];
-					added.add(i);
-				}
-			});
-			for (var i = 0; i < lines.length; i++) {
-				if (!added.has(i)) {
-					lines[i] = ' ' + lines[i];
-				}
-			}
-			env.code = lines.join('\n');
-		}
-		if (LANGUAGE_REGEX.test(lang) && !env.grammar) {
-			env.grammar = Prism.languages[lang] = Prism.languages.diff;
-		}
-	});
-	Prism.hooks.add('before-tokenize', function (env) {
-		if (!warningLogged && !Prism.languages.diff && !Prism.plugins.autoloader) {
-			warningLogged = true;
-			console.warn("Prism's Diff Highlight plugin requires the Diff language definition (prism-diff.js)." +
-				"Make sure the language definition is loaded or use Prism's Autoloader plugin.");
-		}
+    if (env.language !== 'diff') {
+      const langMatch = LANGUAGE_REGEX.exec(env.language);
+      if (!langMatch) {
+        return; // not a language specific diff
+      }
 
-		var lang = env.language;
-		if (LANGUAGE_REGEX.test(lang) && !Prism.languages[lang]) {
-			Prism.languages[lang] = Prism.languages.diff;
-		}
-	});
+      diffLanguage = langMatch[1];
+      diffGrammar = Prism.languages[diffLanguage];
+    }
 
-	Prism.hooks.add('wrap', function (env) {
-		var diffLanguage; var diffGrammar;
+    const PREFIXES = Prism.languages.diff && Prism.languages.diff.PREFIXES;
 
-		if (env.language !== 'diff') {
-			var langMatch = LANGUAGE_REGEX.exec(env.language);
-			if (!langMatch) {
-				return; // not a language specific diff
-			}
+    // one of the diff tokens without any nested tokens
+    if (PREFIXES && env.type in PREFIXES) {
+      /** @type {string} */
+      const content = env.content.replace(HTML_TAG, ''); // remove all HTML tags
 
-			diffLanguage = langMatch[1];
-			diffGrammar = Prism.languages[diffLanguage];
-		}
+      /** @type {string} */
+      const decoded = content.replace(/&lt;/g, '<').replace(/&amp;/g, '&');
 
-		var PREFIXES = Prism.languages.diff && Prism.languages.diff.PREFIXES;
+      // remove any one-character prefix
+      const code = decoded.replace(/(^|[\r\n])./g, '$1');
 
-		// one of the diff tokens without any nested tokens
-		if (PREFIXES && env.type in PREFIXES) {
-			/** @type {string} */
-			var content = env.content.replace(HTML_TAG, ''); // remove all HTML tags
+      // highlight, if possible
+      let highlighted;
+      if (diffGrammar) {
+        highlighted = Prism.highlight(code, diffGrammar, diffLanguage);
+      } else {
+        highlighted = Prism.util.encode(code);
+      }
 
-			/** @type {string} */
-			var decoded = content.replace(/&lt;/g, '<').replace(/&amp;/g, '&');
+      // get the HTML source of the prefix token
+      const prefixToken = new Prism.Token('prefix', PREFIXES[env.type], [/\w+/.exec(env.type)[0]]);
+      const prefix = Prism.Token.stringify(prefixToken, env.language);
 
-			// remove any one-character prefix
-			var code = decoded.replace(/(^|[\r\n])./g, '$1');
+      // add prefix
+      const lines = []; let m;
+      HTML_LINE.lastIndex = 0;
+      while ((m = HTML_LINE.exec(highlighted))) {
+        lines.push(prefix + m[0]);
+      }
+      if (/(?:^|[\r\n]).$/.test(decoded)) {
+        // because both "+a\n+" and "+a\n" will map to "a\n" after the line prefixes are removed
+        lines.push(prefix);
+      }
+      env.content = lines.join('');
 
-			// highlight, if possible
-			var highlighted;
-			if (diffGrammar) {
-				highlighted = Prism.highlight(code, diffGrammar, diffLanguage);
-			} else {
-				highlighted = Prism.util.encode(code);
-			}
-
-			// get the HTML source of the prefix token
-			var prefixToken = new Prism.Token('prefix', PREFIXES[env.type], [/\w+/.exec(env.type)[0]]);
-			var prefix = Prism.Token.stringify(prefixToken, env.language);
-
-			// add prefix
-			var lines = []; var m;
-			HTML_LINE.lastIndex = 0;
-			while ((m = HTML_LINE.exec(highlighted))) {
-				lines.push(prefix + m[0]);
-			}
-			if (/(?:^|[\r\n]).$/.test(decoded)) {
-				// because both "+a\n+" and "+a\n" will map to "a\n" after the line prefixes are removed
-				lines.push(prefix);
-			}
-			env.content = lines.join('');
-
-			if (diffGrammar) {
-				env.classes.push('language-' + diffLanguage);
-			}
-		}
-	});
-
+      if (diffGrammar) {
+        env.classes.push(`language-${diffLanguage}`);
+      }
+    }
+  });
 }());
